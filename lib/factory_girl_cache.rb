8bc171772f7be6aee591e0da78fd54941e8284a6
akm <akm2000@gmail.com>
2017-11-30 18:00:36 +0900

    :+1: Implement

--- lib/factory_girl_cache.rb
+++ lib/factory_girl_cache.rb
@@ -1,5 +1,30 @@
 require "factory_girl_cache/version"
 
+require "factory_girl"
+
 module FactoryGirlCache
-  # Your code goes here...
+  module_function
+
+  def clear
+    @caches = nil
+  end
+
+  def name_for(base_name, key)
+    "#{base_name}_#{key}"
+  end
+
+  def caches
+    @caches ||= {}
+  end
+
+  def of(base_name)
+    caches[base_name.to_sym] ||= Hash.new do |hash, key|
+      hash[key] = FactoryGirl.create( name_for(base_name, key) )
+    end
+  end
+
+  # @return [Proc] a Proc which returns mapped id for given argument
+  def id_map_of(base_name)
+    lambda{|key| of(base_name)[key].id }
+  end
 end
